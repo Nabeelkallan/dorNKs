db211493b30ea78d38affc52ea204c2b0c7ba278
NABEEL KALLAN <104316936+Nabeelkallan@users.noreply.github.com>
2024-10-08 12:23:50 +0530

    Update dornks.py

--- dornks.py
+++ dornks.py
@@ -1,1 +1,8 @@
-python dork_compiler.py
+import os
+import subprocess
+
+# Get the path to the dork_compiler.py file
+script_path = os.path.join(os.path.dirname(__file__), 'dork_compiler.py')
+
+# Run the dork_compiler.py script
+subprocess.run(['python', script_path])
